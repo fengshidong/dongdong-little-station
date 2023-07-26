a98e1d054cb0726e422a288556e00fd830dbaa3b
fengshidong <fengdongdongxuezhang@gmail.com>
2023-07-26 15:23:30 +0800

    node.js

--- demo/node.js/ShangGuiGu/2-http服务/index.js
+++ demo/node.js/ShangGuiGu/2-http服务/index.js
@@ -2,20 +2,20 @@
 const http = require('http')
 // 创建服务对象
 const server = http.createServer((request,response) => {
-  // response.setHeader('content-type','text/html;charset=utf-8')
-  // response.end('你好,世界') //设置响应体
-  // 获取请求方法
-  // console.log(request.method);
-  // console.log(request.url);
-  // console.log(request.headers);
-  // let body = ''
-  // request.on('data',chunk => {
-  //   body += chunk
-  // })
-  // request.on('end',() => {
-  //   console.log(body);
-  //   response.end('HelloHttp')
-  // })
+  response.setHeader('content-type','text/html;charset=utf-8')
+  response.end('你好,世界') //设置响应体
+  获取请求方法
+  console.log(request.method);
+  console.log(request.url);
+  console.log(request.headers);
+  let body = ''
+  request.on('data',chunk => {
+    body += chunk
+  })
+  request.on('end',() => {
+    console.log(body);
+    response.end('HelloHttp')
+  })
   response.end('123234')
 });
 
---
+++ demo/node.js/ShangGuiGu/commonJS/01-npm/index.js
@@ -0,0 +1,4 @@
+const uniq = require('uniq')
+let arr = [1,2,3,4,5,4,3,2,1]
+const res = uniq(arr)
+console.log(res);
